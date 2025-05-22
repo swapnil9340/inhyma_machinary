b78c1401299c90a2fcfcce32ea597f327b830863
swapnil <swapnilchauhan08@gmail.com>
2025-05-22 10:44:32 +0530

    url

--- src/components/ProductDescription/ProductDescriptionBanner.jsx
+++ src/components/ProductDescription/ProductDescriptionBanner.jsx
@@ -8,8 +8,6 @@ import machine2 from "../../../public/pd2.png"
 
 const ProductDescriptionBanner = ({details}) => {
 
-  console.log("========>" ,details)
-
   const bannerContent = {
     heading1 : { subHeading1 : details?.name ,subHeading2 : "Hygiene & Longevity"},
     heading2 : "Explore INHYMA’s advanced vacuum packaging solutions built to extend product shelf life, prevent contamination, and ensure airtight sealing  across food, pharma, chemical, and logistics industries.",
--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api', // Change as needed
+//   baseURL: 'http://localhost:3000/api', // Change as needed
+  baseURL: 'https://inhyma-machinary-git-master-swapnil9340s-projects.vercel.app/api', // Change as needed
   headers: {
     'Content-Type': 'application/json',
   },
