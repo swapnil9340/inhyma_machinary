32a17ae716e927755a5293e58b63394129aed36c
Ajay <ajaykushwaha626162@gmail.com>
2025-05-24 22:41:29 +0530

    fix code

--- src/components/ProductCategory/SelectCategory.jsx
+++ src/components/ProductCategory/SelectCategory.jsx
@@ -2,37 +2,36 @@ import ChooseCategory from "./ChooseCategory";
 import Cards from "./Cards";
 import { Container, Grid } from "@mui/material";
 import { useEffect, useState } from "react";
-import { set } from "mongoose";
 
 
-const SelectCategory = ({allProducts ,allCategories}) => {
-  const [selectedCategory , setSelectedCategory ] = useState({});
-  const [filteredCards , setFilteredCards] = useState([]);
+const SelectCategory = ({ allProducts, allCategories }) => {
+  const [selectedCategory, setSelectedCategory] = useState({});
+  const [filteredCards, setFilteredCards] = useState([]);
 
 
-  useEffect(()=>{
-    if(allCategories.length){
-      setSelectedCategory({["Carton Sealer"] : true})
+  useEffect(() => {
+    if (allCategories.length) {
+      setSelectedCategory({ [allCategories[0].name]: true })
     }
-  } , [JSON.stringify(allCategories)])
+  }, [JSON.stringify(allCategories)])
 
   console.log(allProducts)
-  useEffect(()=>{
-    if(Object.keys(selectedCategory).length){
-      const result = allProducts.filter((product)=> Object.keys(selectedCategory).includes(product.category) && selectedCategory[product.category] === true  );
+  useEffect(() => {
+    if (Object.keys(selectedCategory).length) {
+      const result = allProducts.filter((product) => Object.keys(selectedCategory).includes(product.category) && selectedCategory[product.category] === true);
       setFilteredCards(result);
     }
-    
-  } ,[JSON.stringify(selectedCategory)])
+
+  }, [JSON.stringify(selectedCategory)])
 
   return (
     <>
       <Container maxWidth="xl">
-        <Grid container sx={{padding : "20px 0"}}  spacing={4}>
-          <Grid size={{xs:12 , md:3}} sx={{width : "100%"}} >
+        <Grid container sx={{ padding: "20px 0" }} spacing={4}>
+          <Grid size={{ xs: 12, md: 3 }} sx={{ width: "100%" }} >
             <ChooseCategory allCategories={allCategories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
           </Grid>
-          <Grid size={{xs:12 , md:8}} >
+          <Grid size={{ xs: 12, md: 8 }} >
             <Cards filteredCards={filteredCards} />
           </Grid>
         </Grid>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.css";
+import "../pages/index.css"
 
 export default function App({ Component, pageProps }) {
   return (
