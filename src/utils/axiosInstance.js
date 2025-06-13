1125d688a0be38b2ee6de986a37ba7640fbbaaae
swapnil9340 <95558311+swapnil9340@users.noreply.github.com>
2025-06-13 12:00:30 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://inhyma-machinary-git-master-swapnil9340s-projects.vercel.app/api',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://www.inhyma.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
